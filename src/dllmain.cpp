48fd30e83146ae51f766491567dd26e1d63d4b6f
matcool <26722564+matcool@users.noreply.github.com>
2021-03-10 14:22:39 -0300

    Oh my god im stupid

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -27,8 +27,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
             CreateThread(0, 0x1000, myThread, hModule, 0, 0);
-        case DLL_THREAD_ATTACH:
-        case DLL_THREAD_DETACH:
+            break;
         case DLL_PROCESS_DETACH:
             Hooks::unload();
             break;
